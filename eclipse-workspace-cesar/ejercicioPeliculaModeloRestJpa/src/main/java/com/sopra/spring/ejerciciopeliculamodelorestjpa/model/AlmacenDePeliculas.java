db4a064bf900bb45ac3cda37f3f11cd19f78d7a6
cegasa89 <cegasa89@gmail.com>
2019-07-15 14:55:15 +0200

    por si

--- eclipse-workspace-cesar/ejercicioPeliculaModeloRestJpa/src/main/java/com/sopra/spring/ejerciciopeliculamodelorestjpa/model/AlmacenDePeliculas.java
+++ eclipse-workspace-cesar/ejercicioPeliculaModeloRestJpa/src/main/java/com/sopra/spring/ejerciciopeliculamodelorestjpa/model/AlmacenDePeliculas.java
@@ -1,11 +1,12 @@
 package com.sopra.spring.ejerciciopeliculamodelorestjpa.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sopra.spring.ejerciciopeliculamodelorestjpa.entities.Pelicula;
 
 
-
+@Repository
 public interface AlmacenDePeliculas extends JpaRepository<Pelicula, Integer> {
 
 }
